93511ee3e8310cf335ea44640382df6d116ada93
syzer <syzer3@gmail.com>
2015-04-09 21:07:16 +0200

    example using TRHU( ) and reducing on remote worker AKA better then mongoDb

--- nGram.js
+++ nGram.js
@@ -1,7 +1,7 @@
 /**
  * Created by syzer on 4/7/2015.
  */
-var jsSpark = require('js-spark')({workers: 8});
+var jsSpark = require('js-spark')({workers: 1});
 var task = jsSpark.jsSpark;
 var q = jsSpark.q;
 var _ = require('lodash');
@@ -52,12 +52,35 @@ function mergeBig(texts) {
             .thru(bigramText)
             .run()
     })).then(function reducer(data) {
-        //return task(data).add('merge').run();
-        return _.mergeObjectsInArr(data);
+        //return _.mergeObjectsInArr(data);     // uncomment if u want to reduce on this worker
+        return task(data)
+            .thru(merger)
+            .run();
     }).then(function cacheInDb(data) {
         dataBase = data;
         return data;
     });
+}
+
+function merger(arr) {
+    var _ = this;
+
+    function mergeObjectsInArr(arr) {
+        return arr.reduce(function (acc, curr) {
+            return _.merge(acc, curr, objectMerger);
+        });
+    }
+
+    function objectMerger(a, b) {
+        if (a && b && _.isNumber(a) && _.isNumber(b)) {
+            return a + b;
+        }
+        if (_.isArray(a)) {
+            return a.concat(b);
+        }
+    }
+
+    return mergeObjectsInArr(arr);
 }
 
 function bigramText(str) {
@@ -98,7 +121,7 @@ module
     .train(DRACULA + 'full.txt', '\r\n\r\n\r\nCHAPTER')
     .then(function () {
         var result = predict('i').slice(0, 10);
-        console.log('predictions for word `i`', result);
+        console.log('predictions for word `i`:\n', result);
 
         console.timeEnd('train+predict');
     });
